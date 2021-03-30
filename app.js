bae2ba2563f9fd92d88f48307d2f50fe23f79f23
Tony <tony.d.nguyen@protonmail.com>
2021-03-30 09:34:20 -0500

    Successfully implemented background color generator.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var generatorColor = function () {
-    console.log("Hello World");
+    var red = getRandomNumber();
+    var green = getRandomNumber();
+    var blue = getRandomNumber();
+    document.body.style.background = "rgba(" + red + ", " + green + ", " + blue + ")";
+    document.getElementById("display-color").innerHTML = "rgba(" + red + ", " + green + ", " + blue + ")";
+};
+var getRandomNumber = function () {
+    return Math.random() * 255;
 };
--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
 const generatorColor = (): void => {
-    console.log("Hello World");
+    let red: number = getRandomNumber();
+    let green: number = getRandomNumber();
+    let blue: number = getRandomNumber();
+
+    document.body.style.background = `rgba(${red}, ${green}, ${blue})`;
+    document.getElementById("display-color").innerHTML = `rgba(${red}, ${green}, ${blue})`;
+}
+
+const getRandomNumber = (): number => {
+    return Math.random() * 255;
 }
